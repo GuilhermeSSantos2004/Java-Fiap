ab28773b7dd1c0a5bec165733378f61cf3746323
guilherme <guilhermesilvas2004@gmail.com>
2024-02-08 22:22:02 -0300

    if else if and else

---
+++ Aula1/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Texto Fixo");
+
+        String texto = input.nextLine();
+
+        System.out.println(texto);
+
+        int valor = input.nextInt();
+        System.out.println("Valor: " + valor);
+
+        System.out.println("V1 = ");
+        int v1 = input.nextInt();
+
+        System.out.println("V2 = ");
+        int v2 = input.nextInt();
+
+        if (v1 > v2) {
+            System.out.println("V1 é maior")
+        }
+        else if {
+            Sysyem.out.println("V2 é maior")
+        }
+    else {
+            System.out.println("V1 e V2 são iguais")
+        }
+
+
+
+    }
+
+}
